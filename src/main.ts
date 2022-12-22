0c057980245d9045b1a31f56e0b909adc4cd9e86
Andy Allison <AndrewAllison@users.noreply.github.com>
2022-12-22 11:31:12 +0000

    feat(init): initial commit
    First of many

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const port = process.env.PORT || 6000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+
+bootstrap()
+  .catch((error) => console.error(error))
+  .then(() => console.log(`Listening on http://localhost:${port}`));
